5ad646e8d2ce3d4fd9f3bea802ee3323db4fbb8e
qitas <qitas@qitas.cn>
2021-01-14 16:17:53 +0800

    fix

--- examples/spl-blink/src/main.c
+++ examples/spl-blink/src/main.c
@@ -1,44 +1,13 @@
-/**
-  ******************************************************************************
-  * @file    GPIO_Toggle\main.c
-  * @author  MCD Application Team
-  * @version V2.0.4
-  * @date    26-April-2018
-  * @brief   This file contains the main function for GPIO Toggle example.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
-  *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */ 
-
 /* Includes ------------------------------------------------------------------*/
 #include "stm8s.h"
 #include "stm8s_it.h"    /* SDCC patch: required by SDCC for interrupts */
 
-/**
-  * @addtogroup GPIO_Toggle
-  * @{
-  */
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 /* Evalboard I/Os configuration */
 
-#define LED_GPIO_PORT  (GPIOG)
+#define LED_GPIO_PORT  (GPIOA)
 #define LED_GPIO_PINS  (GPIO_PIN_3 | GPIO_PIN_2 | GPIO_PIN_1 | GPIO_PIN_0)
 
 /* Private macro -------------------------------------------------------------*/
@@ -93,7 +62,7 @@ void Delay(uint16_t nCount)
   * @retval None
   */
 void assert_failed(uint8_t* file, uint32_t line)
-{ 
+{
   /* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
