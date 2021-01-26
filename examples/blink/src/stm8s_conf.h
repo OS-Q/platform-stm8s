4baf262a009b15f248ef156f6c7b7c182330d1ef
qitas <qitas@qitas.cn>
2021-01-26 18:22:56 +0800

    fix

--- examples/blink/src/stm8s_conf.h
+++ examples/blink/src/stm8s_conf.h
@@ -6,18 +6,8 @@
 /* Includes ------------------------------------------------------------------*/
 #include "stm8s.h"
 
-#include "stm8s_clk.h"
-// #include "stm8s_spi.h"
-// #include "stm8s_uart1.h"
+#include "stm8s_test.h"
 
-
-#define USE_FULL_ASSERT    (0)
-#ifdef  USE_FULL_ASSERT
-#define assert_param(expr) ((expr) ? (void)0 : assert_failed((uint8_t *)__FILE__, __LINE__))
-void assert_failed(uint8_t* file, uint32_t line);
-#else
-#define assert_param(expr) ((void)0)
-#endif
 
 #endif /* __STM8S_CONF_H */
 
