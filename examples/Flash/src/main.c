fbf1129062702873160f22ec6c4765b6543779a3
qitas <qitas@qitas.cn>
2021-01-27 14:58:45 +0800

    add

--- examples/Flash/src/main.c
+++ examples/Flash/src/main.c
@@ -1,7 +1,14 @@
+/*******************************************************************************
+****版本：V1.0.0
+****平台：STM8S
+****日期：2021-01-12
+****作者：Qitas
+****版权：OS-Q
+*******************************************************************************/
+
 #include <stdio.h>
 #include <string.h>
-#include <stm8s.h>
-#include <uart.h>
+#include "main.h"
 
 /* Use last 64 bytes of flash for user data */
 #define ID_ADDR                 (0x8000 + 0x1FC0)
@@ -12,50 +19,26 @@
 __at(ID_ADDR) const uint8_t id = 42;
 __at(USER_DATA_ADDR) uint8_t data[8];
 
-int putchar(int c)
-{
-    uart_write(c);
-    return 0;
-}
 
-/**
- * Enable write access to flash memory
- */
-static void flash_unlock() {
-    FLASH_PUKR = FLASH_PUKR_KEY1;
-    FLASH_PUKR = FLASH_PUKR_KEY2;
-    while (!(FLASH_IAPSR & (1 << FLASH_IAPSR_PUL)));
-}
-
-/**
- * Disable write access to flash memory
- */
-static inline void flash_lock() {
-    FLASH_IAPSR &= ~(1 << FLASH_IAPSR_PUL);
-}
-
-/**
- * Wait for flash programming to finish
- * (note: not necessary on devices without RWW support)
- */
-static inline void flash_busy_wait() {
-    while (!(FLASH_IAPSR & (1 << FLASH_IAPSR_EOP)));
-}
 
 void main()
 {
-    uart_init();
-    printf("\nID = %d. Data[0] = %d\n", id, data[0]);
+    clk_init();
+    uart1_init(0,0);
+    // printf("\nID = %d. Data[0] = %d\n", id, data[0]);
     flash_unlock();
-    for (int i = 0; i < sizeof(data); i++) {
-        /* same as _MEM_(USER_DATA_ADDR + i) += id + i */
+    for (int i = 0; i < sizeof(data); i++)
+    {
         data[i] += id + i;
     }
     flash_lock();
-
-    printf("Data[0] = %d\n", data[0]);
-
-    while (1) {
-
+    // printf("Data[0] = %d\n", data[0]);
+    while (1)
+    {
+        LED1_TOG;
+        delay_ms(300);
     }
 }
+
+
+/*---------------------------(C) COPYRIGHT 2021 OS-Q -------------------------*/
---
+++ examples/Flash/src/main.h
@@ -0,0 +1,12 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm8s.h"
+#include "delay.h"
+#include "init.h"
+#include "pin.h"
+#include "uart1.h"
+#include "flash.h"
+
+#endif
+
