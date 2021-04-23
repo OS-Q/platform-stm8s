4c50b0bacaa42dce1fb86abaf6b2680e5485be7e
qitas <qitas@qitas.cn>
2021-04-23 18:05:14 +0800

    fix

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 
 [![sites](http://182.61.61.133/link/resources/OSQ.png)](http://www.OS-Q.com)
 
-[![Build Status](https://github.com/OS-Q/P14/workflows/macos/badge.svg)](https://github.com/OS-Q/P14/actions/workflows/macos.yml)
-[![Build Status](https://github.com/OS-Q/P14/workflows/ubuntu/badge.svg)](https://github.com/OS-Q/P14/actions/workflows/ubuntu.yml)
-[![Build Status](https://github.com/OS-Q/P14/workflows/windows/badge.svg)](https://github.com/OS-Q/P14/actions/workflows/windows.yml)
-[![Build Status](https://github.com/OS-Q/P14/workflows/PlatformIO/badge.svg)](https://github.com/OS-Q/P14/actions/workflows/platformio.yml)
+[![Build Status](https://github.com/OS-Q/P14/workflows/CI/badge.svg)](https://github.com/OS-Q/P14/actions/workflows/CI.yml)
+[![Build Status](https://github.com/OS-Q/P14/workflows/CD/badge.svg)](https://github.com/OS-Q/P14/actions/workflows/CD.yml)
+[![Build Status](https://github.com/OS-Q/P14/workflows/nightly/badge.svg)](https://github.com/OS-Q/P14/actions/workflows/nightly.yml)
+[![Build Status](https://circleci.com/gh/OS-Q/P14.svg?style=svg)](https://circleci.com/gh/OS-Q/P14)
 [![Build Status](https://travis-ci.com/OS-Q/P14.svg?branch=master)](https://travis-ci.com/OS-Q/P14)
+[![Build Status](https://cloud.drone.io/api/badges/OS-Q/P14/status.svg)](https://cloud.drone.io/OS-Q/P14)
 [![Build status](https://ci.appveyor.com/api/projects/status/p08jsu3nvloyo7pu?svg=true)](https://ci.appveyor.com/project/Qitas/p14)
 
 ### [简介](https://github.com/OS-Q/P14/wiki)
@@ -17,8 +18,6 @@
 
 产品丰富，低成本，集成EEPROM，使用SDCC编译。
 
-* 支持 Arduino [![Build Status](https://github.com/OS-Q/A14A/workflows/macos/badge.svg)](https://github.com/OS-Q/A14A/actions/workflows/macos.yml)
-[![Build Status](https://github.com/OS-Q/A14A/workflows/ubuntu/badge.svg)](https://github.com/OS-Q/A14A/actions/workflows/ubuntu.yml)
-[![Build Status](https://github.com/OS-Q/A14A/workflows/windows/badge.svg)](https://github.com/OS-Q/A14A/actions/workflows/windows.yml)
+* 支持 Arduino
 
 ### [源圈 OS-Q (Open Source & QA) ](http://www.OS-Q.com)
--- builder/main.py
+++ builder/main.py
@@ -1,9 +1,24 @@
+# Copyright 2018-present PlatformIO <contact@platformio.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import sys
 from os.path import join
 from platform import system
 
-from SCons.Script import (AlwaysBuild, COMMAND_LINE_TARGETS, Default,
-                            DefaultEnvironment)
+from SCons.Script import (AlwaysBuild, Builder, COMMAND_LINE_TARGETS, Default,
+                          DefaultEnvironment)
+
 
 env = DefaultEnvironment()
 board_config = env.BoardConfig()
@@ -15,7 +30,7 @@ env.Replace(
     GDB="stm8-gdb",
     LD="sdldstm8",
     RANLIB="sdranlib",
-    OBJCOPY="sdobjcopy",
+    OBJCOPY="stm8-objcopy",
     SIZETOOL="stm8-size",
     OBJSUFFIX=".rel",
     LIBSUFFIX=".lib",
@@ -42,8 +57,8 @@ env.Replace(
 
     LIBPATH=[
         join(env.PioPlatform().get_package_dir("toolchain-sdcc"),
-            "%s" % "lib" if system() == "Windows" else join("share", "sdcc", "lib"),
-            board_config.get("build.cpu"))
+             "%s" % "lib" if system() == "Windows" else join("share", "sdcc", "lib"),
+             board_config.get("build.cpu"))
     ],
 
     LIBS=["stm8"],
@@ -58,24 +73,35 @@ env.Replace(
     PROGSUFFIX=".elf"
 )
 
-
-def _ldflags_for_ihx(env, ldflags):
-    ldflags = ["--out-fmt-ihx" if f == "--out-fmt-elf" else f for f in ldflags]
-    return ldflags
-
-def _ldflags_for_hex(env, ldflags):
-    ldflags = ["--out-fmt-ihx" if f == "--out-fmt-elf" else f for f in ldflags]
-    return ldflags
-
 env.Append(
     ASFLAGS=env.get("CFLAGS", [])[:],
-    __ldflags_for_ihx=_ldflags_for_ihx,
-    ldflags_for_ihx="${__ldflags_for_ihx(__env__, LINKFLAGS)}"
+    BUILDERS=dict(
+        ElfToHex=Builder(
+            action=env.VerboseAction(" ".join([
+                "$OBJCOPY",
+                "-O",
+                "ihex",
+                "--remove-section=\".debug*\"",
+                "--remove-section=SSEG",
+                "--remove-section=INITIALIZED",
+                "--remove-section=DATA",
+                "$SOURCES",
+                "$TARGET"
+            ]), "Building $TARGET"),
+            suffix=".hex"
+        )
+    )
 )
 
 # Allow user to override via pre:script
 if env.get("PROGNAME", "program") == "program":
     env.Replace(PROGNAME="firmware")
+
+# Automatically remove flags which are incompatible with SDCC in debug mode
+# Can be replaced by `PIODEBUGFLAGS` once PIO Core 5.2 is released
+if env.GetBuildType() == "debug":
+    env.Append(BUILD_UNFLAGS=["-Og", "-g2", "-ggdb2"])
+    env.Append(CFLAGS=["--debug", "--out-fmt-elf"])
 
 #
 # Target: Build executable and linkable firmware
@@ -87,12 +113,8 @@ if "nobuild" in COMMAND_LINE_TARGETS:
     target_firm = join("$BUILD_DIR", "${PROGNAME}.ihx")
 else:
     target_elf = env.BuildProgram()
-    target_firm = env.Command(
-        join("$BUILD_DIR", "${PROGNAME}.ihx"),
-        env['PIOBUILDFILES'],
-        env['LINKCOM'].replace("$LINKFLAGS", "$ldflags_for_ihx")
-    )
-    env.Depends(target_firm, target_elf)
+    target_firm = env.ElfToHex(join("$BUILD_DIR", "${PROGNAME}"), target_elf)
+    env.Depends(target_firm, "checkprogsize")
 
 AlwaysBuild(env.Alias("nobuild", target_firm))
 target_buildprog = env.Alias("buildprog", target_firm, target_firm)
@@ -130,17 +152,20 @@ if upload_protocol == "serial":
 
     upload_actions = [
         env.VerboseAction(env.AutodetectUploadPort,
-                            "Looking for upload port..."),
+                          "Looking for upload port..."),
         env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")
     ]
 
 elif "stlink" in upload_protocol:
     mcu = board_config.get("build.mcu")
+    # either derive value for the "part" switch from MCU name, or use the value
+    # in the board manifest, in cases in which the derivation would be wrong.
+    flash_target = board_config.get("upload.stm8flash_target", mcu[:8] + "?" + mcu[9])
     env.Replace(
         UPLOADER="stm8flash",
         UPLOADERFLAGS=[
             "-c", "$UPLOAD_PROTOCOL",
-            "-p", "%s" % mcu[:8] + "?" + mcu[9],
+            "-p", flash_target,
             "-s", "flash", "-w"
         ],
         UPLOADCMD='"$UPLOADER" $UPLOADERFLAGS $SOURCE'
